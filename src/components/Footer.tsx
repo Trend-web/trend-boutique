5586328482c271bfd11524bf1a1b6d4abaa034f6
Azim <azimakhorajiya@gmail.com>
2024-07-22 23:54:48 +0300

    created navbar and footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,6 @@
+
+export default function Footer() {
+  return (
+    <div className="flex justify-center items-center">WebApp crafted supafast by Azim</div>
+  )
+}
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,14 @@
+
+
+export default function Navbar() {
+  return (
+    <div className="flex justify-between items-center mr-10 ml-10">
+      <div>Menu</div>
+      <div>Logo</div>
+      <div className="flex justify-between gap-8">
+        <div>Account</div>
+        <div>Cart</div>
+      </div>
+    </div>
+  )
+}
