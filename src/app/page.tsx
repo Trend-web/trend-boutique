a53e0a5917fc227c08fa93224ba26bbfe0a07a07
Azim <azimakhorajiya@gmail.com>
2024-07-22 20:35:23 +0300

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1 className="flex justify-center items-center h-screen">Hello</h1>
     </div>
   );
 }
